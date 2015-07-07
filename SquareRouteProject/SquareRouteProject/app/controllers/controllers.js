2a112f79e8002544c48e14b891ad89ff8b4bd048
voltaiaj <alexander.voltaire@gmail.com>
2015-07-07 14:04:14 -0500

    Added Angular GoogleMaps Functionality

--- SquareRouteProject/SquareRouteProject/app/controllers/controllers.js
+++ SquareRouteProject/SquareRouteProject/app/controllers/controllers.js
@@ -64,10 +64,17 @@
     function GoogleMapsController($scope, uiGmapGoogleMapApi) {
         var vm = this;
         vm.message = "GoogleMaps View"
-        $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+
+        var lat = 45;
+        var lng = -73;
+        var zoom = 8;
+
+        $scope.map = { center: { latitude: lat, longitude: lng }, zoom: zoom };
+        
+        //directionsDisplay = new google.maps.DirectionsRenderer();        
 
         uiGmapGoogleMapApi.then(function (maps) {
-
+            //directionsDisplay.setMap(maps);
         })
     }
 
