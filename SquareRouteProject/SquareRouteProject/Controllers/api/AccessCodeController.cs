d0798665f9649e1100a150dd930c16133acf3019
voltaiaj <alexander.voltaire@gmail.com>
2015-07-12 20:36:14 -0500

    Split AdminController into AccessCode/BusStop/Route Controllers

---
+++ SquareRouteProject/SquareRouteProject/Controllers/api/AccessCodeController.cs
@@ -0,0 +1,97 @@
+﻿using SquareRouteProject.Domain.Entities;
+using SquareRouteProject.Infastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace SquareRouteProject.Presentation.Controllers.api
+{
+    [RoutePrefix("api/AccessCode")]
+    public class AccessCodeController : ApiController
+    {
+        private UnitOfWorkDataModels _unitOfWork;
+
+        public AccessCodeController()
+        {
+            _unitOfWork = new UnitOfWorkDataModels();
+        }
+
+        #region AccessCode Methods
+        #region AccessCode CREATE
+        // POST api/AccessCode/AddAccessCode
+        [Route("AddAccessCode")]
+        public IHttpActionResult AddAccessCode(AccessCode accessCode)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.AccessCodeRepository.Repo.Add(accessCode);
+                _unitOfWork.SaveChanges();
+                return Ok();
+            }
+            return BadRequest();
+        }
+        #endregion
+
+        #region AccessCode GET
+        // GET api/AccessCode/GetAccessCodeById
+        [Route("GetAccessCodeById")]
+        public AccessCode GetAccessCodeById(int id)
+        {
+            return _unitOfWork.AccessCodeRepository.GetAccessCodeById(id);
+        }
+
+        // GET api/AccessCode/GetAccessCodeByName
+        [Route("GetAccessCodeByName")]
+        public AccessCode GetAccessCodeByName(string accessCodeName)
+        {
+            return _unitOfWork.AccessCodeRepository.GetAccessCodeByName(accessCodeName);
+        }
+
+        // GET api/AccessCode/GetAccessCodeByRouteId
+        [Route("GetAccessCodeByRouteId")]
+        public AccessCode GetAccessCodeByRouteId(int routeId)
+        {
+            return _unitOfWork.AccessCodeRepository.GetAccessCodeByRouteId(routeId);
+        }
+
+        // GET api/AccessCode/GetAllAccessCodes
+        [Route("GetAllAccessCodes")]
+        public IList<AccessCode> GetAllAccessCodes()
+        {
+            return _unitOfWork.AccessCodeRepository.Repo.GetAll();
+        }
+
+
+        #endregion
+
+        #region AccessCode UPDATE
+        // POST api/AccessCode/UpdateAccessCode
+        [Route("UpdateAccessCode")]
+        public IHttpActionResult UpdateAccessCode(AccessCode accessCode)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.AccessCodeRepository.Repo.Update(accessCode);
+                _unitOfWork.SaveChanges();
+                return Ok();
+            }
+            return BadRequest();
+        }
+
+        #endregion
+
+        #region AccessCode Delete
+        // POST api/AccessCode/DeleteAccessCodeById
+        [Route("DeleteAccessCodeById")]
+        public IHttpActionResult DeleteAccessCodeById(int accessCodeId)
+        {
+            _unitOfWork.AccessCodeRepository.DeleteAccessCodeById(accessCodeId);
+            _unitOfWork.SaveChanges();
+            return Ok();
+        }
+        #endregion
+        #endregion
+    }
+}
---
+++ SquareRouteProject/SquareRouteProject/Controllers/api/BusStopController.cs
@@ -0,0 +1,85 @@
+﻿using SquareRouteProject.Domain.Entities;
+using SquareRouteProject.Infastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace SquareRouteProject.Presentation.Controllers.api
+{
+    [RoutePrefix("api/BusStop")]
+    public class BusStopController : ApiController
+    {
+        private UnitOfWorkDataModels _unitOfWork;
+
+        public BusStopController() 
+        {
+            _unitOfWork = new UnitOfWorkDataModels();
+        }
+        #region BusStop Methods
+        #region BusStop CREATE
+        // POST api/BusStop/AddBusStop
+        [Route("AddBusStop")]
+        public IHttpActionResult AddBusStop(BusStop busStop)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.BusStopRepository.Repo.Add(busStop);
+                _unitOfWork.SaveChanges();
+            }
+            return Ok();
+        }
+        #endregion
+
+        #region BusStop GET
+        // GET api/BusStop/GetBusStopById
+        [Route("GetBusStopById")]
+        public BusStop GetBusStopById(int id)
+        {
+            return _unitOfWork.BusStopRepository.GetStopById(id);
+        }
+
+        // GET api/BusStop/GetBusStopsbyRouteId
+        [Route("GetBusStopsbyRouteId")]
+        public IList<BusStop> GetBusStopsbyRouteId(int id)
+        {
+            return _unitOfWork.BusStopRepository.GetStopsByRouteId(id);
+        }
+
+        // GET api/BusStop/GetAllBusStops
+        [Route("GetAllBusStops")]
+        public IList<BusStop> GetAllBusStops()
+        {
+            return _unitOfWork.BusStopRepository.Repo.GetAll();
+        }
+        #endregion
+
+        #region BusStop UPDATE
+        // POST api/BusStop/UpdateBusStop
+        [Route("UpdateBusStop")]
+        public IHttpActionResult UpdateBusStop(BusStop stop)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.BusStopRepository.Repo.Update(stop);
+                _unitOfWork.SaveChanges();
+                return Ok();
+            }
+            return BadRequest();
+        }
+        #endregion
+
+        #region BusStop DELETE
+        // POST api/BusStop/DeleteBusStopById
+        [Route("DeleteBusStopById")]
+        public IHttpActionResult DeleteBusStopById(int busStopId)
+        {
+            _unitOfWork.BusStopRepository.DeleteBusStopById(busStopId);
+            _unitOfWork.SaveChanges();
+            return Ok();
+        }
+        #endregion
+        #endregion
+    }
+}
