bc645063015ff1e80cb10522efbf9c7110a19f8e
voltaiaj <alexander.voltaire@gmail.com>
2015-07-13 08:39:07 -0500

    Adding AccessCode and Route Repository

---
+++ SquareRouteProject/SquareRouteProject.Infastructure/Repositories/AccessCodeRepository.cs
@@ -0,0 +1,49 @@
+﻿using SquareRouteProject.Domain.Entities;
+using SquareRouteProject.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareRouteProject.Infastructure.Repositories
+{
+    internal class AccessCodeRepository : Repository<AccessCode>, IAccessCodeRepository
+    {
+        internal AccessCodeRepository(ApplicationDbContext context)
+            : base(context)
+        { 
+        }
+
+        //Property Provides access to base Repository<TEntity> members
+        public IRepository<AccessCode> Repo
+        {
+            get { return this; }
+        }        
+
+        //GET AccessCode by AccessCodeId
+        public AccessCode GetAccessCodeById(int accessCodeId)
+        {
+            return Set.FirstOrDefault(x => x.AccessCodeId == accessCodeId);
+        }
+
+        //GET AccessCode by AccessCodeName
+        public AccessCode GetAccessCodeByName(string accessCodeName)
+        {
+            return Set.FirstOrDefault(x => x.AccessCodeName == accessCodeName);
+        }
+
+        //GET AccessCode by RouteId
+        public AccessCode GetAccessCodeByRouteId(int routeId)
+        {
+            return Set.FirstOrDefault(x => x.RouteId == routeId);
+        }
+
+        //DELETE AccessCode by AccessCodeId
+        public void DeleteAccessCodeById(int accessCodeId) 
+        {
+            AccessCode accessCodeToBeDeleted = Set.FirstOrDefault(x => x.AccessCodeId == accessCodeId);
+            Set.Remove(accessCodeToBeDeleted);
+        }
+    }
+}
---
+++ SquareRouteProject/SquareRouteProject.Infastructure/Repositories/RouteRepository.cs
@@ -0,0 +1,57 @@
+﻿using SquareRouteProject.Domain.Models;
+using SquareRouteProject.Domain.Repositories;
+using SquareRouteProject.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareRouteProject.Infastructure.Repositories
+{
+    internal class RouteRepository : Repository<Route>, IRouteRepository
+    {
+        internal RouteRepository(ApplicationDbContext context) 
+            : base(context) 
+        { 
+        }
+
+        //Property Provides access to base Repository<TEntity> members
+        public IRepository<Route> Repo
+        {
+            get { return this; }
+        }
+
+        //GET Route by RouteAccessCodeId
+        public Route GetRouteByAccessCodeId(int accessCodeId)
+        {
+            return Set.FirstOrDefault(x => x.AccessCodeId == accessCodeId);
+        }
+
+        //GET Route by RouteId
+        public Route GetRouteById(int routeId)
+        {
+            return Set.FirstOrDefault(x => x.RouteId == routeId);
+        }
+
+        //GET Route by RouteNum
+        public Route GetRouteByRouteNum(int routeNum)
+        {
+            return Set.FirstOrDefault(x => x.RouteNum == routeNum);
+        }
+        
+        //GET List of Routes by DistrictId
+        public IList<Route> GetRoutesByDistrictId(int districtId)
+        {
+            return Set.Where(x => x.DistrictId == districtId).ToList();
+        }
+
+        //DELETE Route by Id
+        public void DeleteRouteById(int routeId)
+        {
+            var routeToBeDeleted = Set.FirstOrDefault(x => x.RouteId == routeId);
+            Set.Remove(routeToBeDeleted);
+        }
+    }
+    
+}
